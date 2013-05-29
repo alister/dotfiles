2d5a11d66caed1cb236b40531e719717bed6d078
Alister Bulman <abulman@gmail.com>
2013-05-29 20:47:08 +0100

    make the plugin my own

--- oh-my-zsh/custom/plugins/alister/alister.plugin.zsh
+++ oh-my-zsh/custom/plugins/alister/alister.plugin.zsh
@@ -1,18 +1,18 @@
-c() { cd ~/code/$1; }
-_c() { _files -W ~/code -/; }
+c() { cd ~/repo/$1; }
+_c() { _files -W ~/repo -/; }
 compdef _c c
 
 h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
 compdef _h h
 
-# autocorrect is more annoying than helpful
-unsetopt correct_all
+### autocorrect is more annoying than helpful
+##unsetopt correct_all
 
 # a few aliases I like
-alias gs='git status'
+alias st='git status'
 alias gd='git diff'
-alias tlog='tail -f log/development.log'
+#alias tlog='tail -f log/development.log'
 
 # add plugin's bin directory to path
 export PATH="$(dirname $0)/bin:$PATH"
